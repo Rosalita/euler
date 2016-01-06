d54da4acbb6b060f31132e2e795c7c6f23e013e9
Rosie Hamilton <rhamilton@scottlogic.com>
2016-01-06 13:04:29 +0000

    getting closer

--- 4.go
+++ 4.go
@@ -5,24 +5,44 @@ import (
 )
 
 func main(){
-  s:= []int{0}
-  for x := 1; x < 100; x++ {
-     for y := 1; y < 100; y++ {
+//  s:= []int{}
+  for x := 900; x < 1000; x++ {
+     for y := 900; y < 1000; y++ {
        xy := x * y
-       fmt.Printf("xy is %v\n", xy)
+       fmt.Printf("xy is %d\n", xy)
+    //   s = append(s, xy)
+       px:= isPalindrome(xy)
+         if px == true {
 
-
-       for i, v := range s {
-         fmt.Printf("v is %v\n", v)
-         if xy != s[i] {
-           fmt.Printf("%v is not in %v, appending it\n",xy, v)
-            s = append(s, xy)
-            continue
-           }
-           continue
-
+         fmt.Printf("%d is palindrome. Its products are %d and %d\n",xy, x, y)
+         }
        }
      }
 
-  }
+}
+
+func isPalindrome(myInt int) bool{
+      // get the digits and store them in a slice
+      origInt := myInt
+      digits:= []int{}
+      for myInt >= 1{
+
+      aDigit := myInt % 10
+      digits = append(digits, aDigit)
+
+      myInt = myInt - aDigit
+      myInt = myInt / 10
+      }
+      // reverse the digits and store in a new variable
+      reverse := 0
+      for i, _ := range digits{
+        reverse = reverse * 10
+        reverse = reverse + digits[i]
+      }
+     // compare the original digits to the reversed digits
+    // fmt.Printf("reverse is %d origint is %d", reverse, origInt)
+      if reverse == origInt {
+        return true
+      }
+      return false
 }
