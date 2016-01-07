70a074cb8deafa1b3247f9bc558af79f76e0c426
Rosie Hamilton <rhamilton@scottlogic.com>
2016-01-07 13:01:38 +0000

    solved 5 - smallest multiple

---
+++ 5.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 1; i > 0; i++ {
+		if i%1 != 0 {
+			continue
+		}
+		if i%2 != 0 {
+			continue
+		}
+		if i%3 != 0 {
+			continue
+		}
+		if i%4 != 0 {
+			continue
+		}
+		if i%5 != 0 {
+			continue
+		}
+		if i%6 != 0 {
+			continue
+		}
+		if i%7 != 0 {
+			continue
+		}
+		if i%8 != 0 {
+			continue
+		}
+		if i%9 != 0 {
+			continue
+		}
+		if i%10 != 0 {
+			continue
+		}
+		if i%11 != 0 {
+			continue
+		}
+		if i%12 != 0 {
+			continue
+		}
+		if i%13 != 0 {
+			continue
+		}
+		if i%14 != 0 {
+			continue
+		}
+		if i%15 != 0 {
+			continue
+		}
+		if i%16 != 0 {
+			continue
+		}
+		if i%17 != 0 {
+			continue
+		}
+		if i%18 != 0 {
+			continue
+		}
+		if i%19 != 0 {
+			continue
+		}
+		if i%20 != 0 {
+			continue
+		}
+		fmt.Println(i)
+		break
+	}
+}
