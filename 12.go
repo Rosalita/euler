660b72ab4dd4999d0049ca6bee327e5b59d91ed8
Rosie Hamilton <rhamilton@scottlogic.com>
2016-01-15 16:35:53 +0000

    work in progress

--- 12.go
+++ 12.go
@@ -5,12 +5,27 @@ import (
 )
 
 func main() {
-	for i := 1; i < 10; i++ {
-		fmt.Printf("%d: triangle %d\n", i, genTriangle(i))
+  var triangle, count, i, j int64
+	for  i = 11000; i < 12001; i++ {  // 2079 has 320, 5984 has 480,
+		triangle = genTriangle(i)
+    count = 0
+		    for j =1; j <= triangle; j++ {
+					 if triangle % j == 0 {
+						 count ++
+						 if count > 480{
+							 fmt.Printf("i= %d Triangle %d has %d divisors\n", i, triangle, count)
+							 return
+						 }
+					 }
+					 continue
+
+	      }
+
 	}
+	fmt.Println("did not find a suitable triangle")
 }
 
-func genTriangle(n int) int {
+func genTriangle(n int64) int64 {
 	total := n
 	for i := n; i > 0; i-- {
 		if total > i {
