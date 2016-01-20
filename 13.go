45f54a4c09f350a602ddbd8990dca072a5086e74
Rosie Hamilton <rhamilton@scottlogic.com>
2016-01-20 13:05:53 +0000

    work still in progress

--- 13.go
+++ 13.go
@@ -16,16 +16,44 @@ func main() {
 	stringData := string(data)
 	slicesOfRows := strings.Split(stringData, "\n")
 
-	// add the first digit in all the rows
+	// add the last digits of all the numbers
+	sum := addNthDigitFromAllRows(slicesOfRows, 49)
+	fmt.Printf("sum of last digit in all rows is %d\n", sum)
+	tens := 0
+	second := 0
+	//third := 0
 
-	fmt.Printf("sum of first digit in all rows is %d", addNthDigitFromAllRows(slicesOfRows, 0))
+	lastdigit := int(sum % 10)        // get the last digit of the sum
+	sdigit := strconv.Itoa(lastdigit) // convert it to string
 
+	// store the answer as a slice of strings
+	var answer []string
+	answer = append(answer, sdigit)
+
+	// if the sum all the numbers is greater than 10
+	// store the second digit of the sum to be used in next calc
+	if sum > 10 { //422
+		//remove the last digit by subtracting it and dividing by 10
+		tens = (int(sum) - lastdigit) / 10
+		fmt.Printf("tens %d\n", tens)
+		second = tens % 10
+		fmt.Printf("second digit of sum is %d\n", second)
+	}
+	// if the sum of all the numbers is greater than 100
+	// store the third digit of the sum to be used in next calc
+
+	if sum > 100 { //422
+		third := (int(tens) - second) / 10
+		fmt.Printf("third digit of sum is %d\n", third)
+	}
+
+	fmt.Println(string(answer[0]))
 }
 
 func addNthDigitFromAllRows(rows []string, n int) int64 {
 	var sum int64
 	for i := 0; i < 100; i++ {
-		number := getNthDigit(rows[i], 0)
+		number := getNthDigit(rows[i], n)
 		sum = sum + number
 
 	}
