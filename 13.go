8d915d32de3a29cead7813287c6aa11035e898ad
Rosie Hamilton <rhamilton@scottlogic.com>
2016-01-25 13:17:02 +0000

    solved 13 - large sum

--- 13.go
+++ 13.go
@@ -14,45 +14,19 @@ func main() {
 	}
 
 	stringData := string(data)
-	slicesOfRows := strings.Split(stringData, "\n")
+	slicesOfRows := strings.Split(stringData, "\r\n")
 
+	var floatsum float64 = 0
 
-  var sums []string
-  modifier := 1000000000
-	total:= 0
-	for i:= 0; i < 10; i++ {
-		columnsum := int(addNthDigitFromAllRows(slicesOfRows, i))
-		columnsum = columnsum * modifier
-		total = total + columnsum
-		modifier = modifier / 10
-		stringsum := strconv.Itoa(columnsum)
-	// append it to slice of sums
-		sums = append(sums, stringsum)
-	}
-
-	for i:= 0; i < 10; i++ {
-		fmt.Println(slicesOfRows[i])
-		fmt.Println(sums[i])
-	}
- fmt.Printf("total is %d", total)
-
-}
-
-func addNthDigitFromAllRows(rows []string, n int) int64 {
-	var sum int64
 	for i := 0; i < 100; i++ {
-		number := getNthDigit(rows[i], n)
-		sum = sum + number
 
-	}
-	return sum
-}
+		myfloat, err := strconv.ParseFloat(slicesOfRows[i], 64)
+		if err != nil {
+			panic(err)
+		}
 
-func getNthDigit(myString string, n int) int64 {
-	digit := string(myString[n])
-	intdigit, ok := strconv.ParseInt(digit, 10, 0)
-	if ok != nil {
-		panic(ok)
+		floatsum = floatsum + myfloat
 	}
-	return intdigit
+
+	fmt.Printf(" total of floats is %f\n", floatsum)
 }
