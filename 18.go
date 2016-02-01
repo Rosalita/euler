db8e984d1ad33ba93bd6952ec4a450b99171d237
Rosie Hamilton <rhamilton@scottlogic.com>
2016-02-01 16:35:27 +0000

    work in progress

--- 18.go
+++ 18.go
@@ -19,40 +19,116 @@ func main() {
 	stringTriangleData := string(rawTriangleData)
 	triangleRows := strings.Split(stringTriangleData, "\r\n")
 
-	fmt.Printf("triangle row 14: %v\n", triangleRows[14])
-		fmt.Printf("triangle row 13: %v\n", triangleRows[13])
-	//var rowSlice []string
-	rowSlice := strings.Split(triangleRows[14], " ")
-	prevRowSlice := strings.Split(triangleRows[13], " ")
-	// add a 0 on to the end of the slice for prev row so both rows have the same number of values
- prevRowSlice = append(prevRowSlice, "0")
+	for i := 14; i > 0; i-- {
 
-	neswSum := make([]int64, 15) // used to store the sum of north east to south west direction number pairs
-	nwseSum := make([]int64, 15) // used to store the sum of north west to south east direction number pairs
+		largestNeswSum, firstNesw, secondNesw, largestNwseSum, firstNwse, secondNwse, neswpos1, neswpos2, nwsepos1, nwsepos2 := getBiggestSums(i, triangleRows)
 
-// calculate the sums of north east to south west number pairs and store them as int64 in neswSum[]
-	for i, _ := range rowSlice {
-//		fmt.Printf("rowSlice %d is %v\n", i, v)
-		rowInt, err := strconv.ParseInt(rowSlice[i], 10, 64)
-		if err != nil {
-			panic(err)
-		}
-
-		prevRowInt, err := strconv.ParseInt(prevRowSlice[i], 10, 64)
-		if err != nil {
-			panic(err)
-		}
-//		fmt.Printf("prevRowSlice %d is %v\n", i, prevRowSlice[i])
-		total := rowInt + prevRowInt
-		neswSum[i] = total
+		fmt.Printf("for row %d and %d:\n", i, i-1)
+		fmt.Printf("largest sw>ne sum is %d which is %d + %d at positions %d and %d\n", largestNeswSum, firstNesw, secondNesw, neswpos1, neswpos2)
+		fmt.Printf("largest se>nw sum is %d which is %d + %d at positions %d and %d\n", largestNwseSum, firstNwse, secondNwse, nwsepos1, nwsepos2)
 	}
 
-// calculate the sums of north west to south east number pairs and store them as int64 in nwseSum[]
-for i, _ := range rowSlice {
-	  var prevRowInt int64
+	journey := getBiggestNumberPath(9, 14, triangleRows)
+	fmt.Printf("journey is %s", journey)
+}
+
+func getBiggestNumberPath(startPoint, startRow int, triangleRows []string) (journey []string) {
+	nextPoint := 0
+	fmt.Printf("startpoint is %d\n", startPoint)
+	rowSlice := strings.Split(triangleRows[startRow], " ")
+	fmt.Printf("value at start point is %s\n", rowSlice[startPoint])
+	// add the starting value to the journey
+	journey = append(journey, rowSlice[startPoint])
+
+	//convert this value to an int and add it to the total
+	intVal, err := strconv.ParseInt(rowSlice[startPoint], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	var total int64
+	total = total + intVal
+
+	prevRowSlice := strings.Split(triangleRows[startRow-1], " ")
+	// check which of the two values can move to is largest
+	fmt.Printf("can move to %s or %s\n", prevRowSlice[startPoint-1], prevRowSlice[startPoint])
+	// convert these string values to int64
+	option1, err := strconv.ParseInt(prevRowSlice[startPoint-1], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	option2, err := strconv.ParseInt(prevRowSlice[startPoint], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	if option1 > option2 {
+		journey = append(journey, prevRowSlice[startPoint-1])
+		nextPoint = startPoint - 1
+	} else {
+		journey = append(journey, prevRowSlice[startPoint])
+		nextPoint = startPoint
+	}
+
+	// check the next point - possibly start looping back through points here
+	fmt.Printf("next point is %d\n", nextPoint)
+	nextSlice := strings.Split(triangleRows[startRow-1], " ")
+	fmt.Printf("value at next point is %s\n", nextSlice[nextPoint])
+	nextPrevRowSlice := strings.Split(triangleRows[startRow-2], " ")
+	// check which of the two values can move to is largest
+	fmt.Printf("can move to %s or %s\n", nextPrevRowSlice[nextPoint-1], nextPrevRowSlice[nextPoint])
+	// convert these string values to int64
+	option1, err = strconv.ParseInt(nextPrevRowSlice[nextPoint-1], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	option2, err = strconv.ParseInt(nextPrevRowSlice[nextPoint], 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	if option1 > option2 {
+		journey = append(journey, nextPrevRowSlice[nextPoint-1])
+		nextPoint = nextPoint - 1
+	} else {
+		journey = append(journey, nextPrevRowSlice[nextPoint])
+	}
+
+	return journey
+}
+
+func getBiggestSums(startRow int, triangleRows []string) (neswSum, firstNesw, secondNesw, nwseSum, firstNwse, secondNwse int64, neswpos1, neswpos2, nwsepos1, nwsepos2 int) {
+
+	//make rowSlice which is a slice containing all the numbers on the startRow of triangleRows
+	rowSlice := strings.Split(triangleRows[startRow], " ")
+	//make prevRowSlice which is a slice of all the numbers in the previous row of triangleRows
+	prevRowSlice := strings.Split(triangleRows[startRow-1], " ")
+	// add a 0 on to the end of the previous row slice so both slices have the same number of values
+	prevRowSlice = append(prevRowSlice, "0")
+
+	neswSumSlice := make([]int64, 15) // used to store the sum of north east to south west direction number pairs
+	nwseSumSlice := make([]int64, 15) // used to store the sum of north west to south east direction number pairs
+
+	// calculate the sums of north east to south west number pairs and store them as int64 in neswSum[]
+	for i, _ := range rowSlice {
+
+		rowInt, err := strconv.ParseInt(rowSlice[i], 10, 64) // convert the numeric string to an int64 and store in rowInt
+		if err != nil {
+			panic(err)
+		}
+
+		prevRowInt, err := strconv.ParseInt(prevRowSlice[i], 10, 64) // convert the numeric string to an int64 and store in prevRowInt
+		if err != nil {
+			panic(err)
+		}
+
+		total := rowInt + prevRowInt // add the two ints together
+		neswSumSlice[i] = total      // and store the sum in neswSumSlice
+	}
+
+	// calculate the sums of north west to south east number pairs and store them as int64 in nwseSum[]
+	for i, _ := range rowSlice {
+		var prevRowInt int64
 		var err error
 
-		rowInt, err := strconv.ParseInt(rowSlice[i], 10, 64)
+		rowInt, err := strconv.ParseInt(rowSlice[i], 10, 64) // convert the numeric string to an int64 and store in rowInt
 
 		if err != nil {
 			panic(err)
@@ -67,50 +143,76 @@ for i, _ := range rowSlice {
 				panic(err)
 			}
 		}
-   total := rowInt + prevRowInt
-	 nwseSum[i] = total
-}
+		total := rowInt + prevRowInt
+		nwseSumSlice[i] = total
+	}
 
-var largestNesw int64 = 0
-var largestNwse int64 = 0
-firstNesw := " "
-secondNesw := " "
-firstNwse := " "
-secondNwse := " "
+	var largestNesw int64 = 0
+	var largestNwse int64 = 0
+	sfirstNesw := " "
+	ssecondNesw := " "
+	sfirstNwse := " "
+	ssecondNwse := " "
+	neswPos1, neswPos2, nwsePos1, nwsePos2 := 0, 0, 0, 0
 
-//find the largest nesw value
-for i, v := range neswSum {
-	if i == 0{
-		largestNesw = v
-		firstNesw = rowSlice[i]
-		secondNesw = prevRowSlice[i]
-	} else {
-		if v > largestNesw {
-			largestNesw = v
-			firstNesw = rowSlice[i]
-			secondNesw = prevRowSlice[i]
+	//find the largest nesw value in the neswSumSlice
+	for i, v := range neswSumSlice {
+		if i == 0 {
+			largestNesw = v          // remember the largest value
+			sfirstNesw = rowSlice[i] //and the numbers that were added to make that value
+			neswPos1 = i
+			ssecondNesw = prevRowSlice[i] // these numbers are actually strings
+			neswPos2 = i
+		} else {
+			if v > largestNesw {
+				largestNesw = v
+				sfirstNesw = rowSlice[i]
+				neswPos1 = i
+				ssecondNesw = prevRowSlice[i]
+				neswPos2 = i
+			}
 		}
 	}
-}
 
-//find the largest nwse value
-for i, v := range nwseSum {
-	if i == 0{
-		largestNwse = v
-		firstNwse = rowSlice[i]
-		secondNwse = "0"
-	} else {
-		if v > largestNwse {
-			largestNwse = v
-			firstNwse = rowSlice[i]
-			secondNwse = prevRowSlice[i-1]
+	//find the largest nwse value in the nwseSumSlice
+	for i, v := range nwseSumSlice {
+		if i == 0 {
+			largestNwse = v // remember the largest value
+			sfirstNwse = rowSlice[i]
+			nwsePos1 = i
+			ssecondNwse = "0"
+			nwsePos2 = i
+		} else {
+			if v > largestNwse {
+				largestNwse = v
+				sfirstNwse = rowSlice[i]
+				nwsePos1 = i
+				ssecondNwse = prevRowSlice[i-1]
+				nwsePos2 = i - 1
 
+			}
 		}
 	}
-}
-	fmt.Printf("largest sum of nesw is %d \n", largestNesw)
-	fmt.Printf("this is the sum of %s and %s\n", firstNesw, secondNesw)
-	fmt.Printf("largest sum of nwse is %d \n", largestNwse)
-	fmt.Printf("this is the sum of %s and %s\n", firstNwse, secondNwse)
 
+	// convert all the numeric strings to int64 before returning them
+	ifirstNesw, err := strconv.ParseInt(sfirstNesw, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	isecondNesw, err := strconv.ParseInt(ssecondNesw, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	ifirstNwse, err := strconv.ParseInt(sfirstNwse, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	isecondNwse, err := strconv.ParseInt(ssecondNwse, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	// return largest sum northeast to southwest and the two values that make this sum
+	// also return the largest sum northwest to southeast and the two values that make this sum
+	// return the position 0 - startRow of the numbers which when added make the largest sum
+	return largestNesw, ifirstNesw, isecondNesw, largestNwse, ifirstNwse, isecondNwse, neswPos1, neswPos2, nwsePos1, nwsePos2
 }
