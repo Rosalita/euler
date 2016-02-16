cafb7e284053f609f9b904be30f02599677dbb3b
Rosie Hamilton <rhamilton@scottlogic.com>
2016-02-16 14:00:40 +0000

    work in progress

---
+++ 24.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main(){
+
+//number of permutations for 0123456789
+// 10 digits which can be arranged
+//  so there are 10! permutations
+// 10! == 3628800
+
+//try to count up from the smallest to the largest value
+//0123456789 - index 1 - changed 2
+//0123456798 - index 2 - changed 2
+//0123456879 - index 3 - changed 3
+//0123456897 - index 4 - changed 3
+//0123456978 - index 5 - changed 3
+//0123456987 - index 6 - changed 3
+//0123457689 - index 7 - changed 4
+//0123457869 - index 8 - changed 4
+//0123457896 - index 9 - changed 4
+//0123457968 - index 10 - changed 4
+//0123457986 - index 11 - changed 4
+
+}
