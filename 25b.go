319c9b9262c6970f752e8ed98ef2e82f984f04ba
Rosie Hamilton <rhamilton@scottlogic.com>
2016-02-22 12:59:06 +0000

    solved 25 - 1000 digit fibonacci numbers

--- 25b.go
+++ 25b.go
@@ -1,39 +1,18 @@
 package main
 
 import(
-"fmt"
+  "fmt"
   "math/big"
 )
 
-
-
 func main(){
-  //n := 5
 a, a2, b  := big.NewInt(int64(0)), big.NewInt(int64(0)), big.NewInt(int64(1))
-
-a2.Set(a)
-a.Set(b)
-b.Add(a2, b)
-fmt.Println(a)
-
-
-
-//var a, b, i float64 = 0, 1, 1
-//for i =1; i <= f.i; i ++ {
-//  a, b = b, a + b
-//a2 = a
-//a = b
-//b = a2 + b
-
-
-
-//}
-//f.n = a
-//f.s = strconv.FormatFloat(f.n, 'f', -1, 64)
-//return
-
+index := 0
+for len(a.String()) < 1000{
+  a2.Set(a)
+  a.Set(b)
+  b.Add(a2, b)
+  index ++
 }
-
-//func fib(){
-
-//}
+ fmt.Printf("index of first term to contain 1000 digits is %d", index)
+}
