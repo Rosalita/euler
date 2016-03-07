80ec8de682bef9c88808b761c7f266f94aec7509
Rosie Hamilton <rhamilton@scottlogic.com>
2016-03-07 13:39:50 +0000

    works for regular size values, still needs work to cope with really big numbers

--- 29.go
+++ 29.go
@@ -1,44 +1,49 @@
 package main
 
 import (
-  "fmt"
-  "math"
-  "sort"
+	"fmt"
+	"math"
+	"sort"
 )
 
-func main(){
-  var start, end float64 = 2, 5
-  var terms []float64
+func main() {
+	var start, end float64 = 2, 5
+	var terms []float64
 
-  for i := start; i<= end; i ++{  // loop through all integers
-    for j := start; j<= end; j++ { // loop through all powers
-      fmt.Println(i, j)
-      pow := math.Pow(i,j)
-      fmt.Println(pow)
-      terms = append(terms, pow)
+	for i := start; i <= end; i++ { // loop through all integers
+		for j := start; j <= end; j++ { // loop through all powers
+			pow := math.Pow(i, j)
+			terms = append(terms, pow)
+		}
+	}
+	sort.Float64s(terms) // sort the terms in numerical order
+	deleted := 0
+	finishflag := false
+	for i, _ := range terms { // for each value in the slice of terms
+		if finishflag == true { // if flag to finish looping is true, use continue to skip any remaining values
+			continue
+		}
+		if i == 0 { // if first value, unable to compare it to previous value so skip it with continue
+			continue
+		}
+		// if anything has been deleted from the original slice, deleted will be > 0
+		// for each deletion the slice will be shorter so...
+		for d := 1; d <= deleted; d++ { //for each time a value was deleted
+			if i == len(terms)-1-d { // if terms[i] would be at the index of (last value) - (number of items deleted)
+				finishflag = true // set finishflag to true
+				continue          // and skip this value
+			}
+		}
 
+		if terms[i] == terms[i-1] { // compare the value to the one before it and if its the same
+			terms = append(terms[:i], terms[i+1:]...) // delete it by setting the slice to all values before and after the term to be removed
+			deleted++ // increment a counter of deleted items
     }
-  }
-  sort.Float64s(terms)
-  fmt.Println(terms)
-
-  for i, v := range terms{
-    deleted := 0
-    if i == 0 {
-      continue
-    }
-    fmt.Println(len(terms))
-    if deleted > 0 && i == len(terms)-1{
-      continue
-    }
-    if terms[i] == terms[i-1]{
-      fmt.Printf("need to delete terms[%d] which is %f\n", i, v )
-      terms2 := append(terms[:i], terms[i+1:]...)
-      fmt.Println("this is the new slice")
-      fmt.Println(terms2)
-      fmt.Println(terms)
-      deleted ++
-    }
-    fmt.Println(i, v)
-  }
+	}
+  	//fmt.Println(terms)
+      count := 0
+      for _, _ = range(terms){
+        count ++
+      }
+      fmt.Printf("There are %d distinct terms\n", count)
 }
